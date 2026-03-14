4796e1eb4def4a34ef5375cebb7fa7d2f8b80ca4
Ayana Dinesh <codewithayana@gmail.com>
2026-03-14 22:37:02 +0530

    feat: initialize React app with Vite and Tailwind CSS
    feat: all setuped

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex place-items-center m-1 border-2 border-blue-500 p-4" style={{ height: '1px', padding: '25px', width: '20%', margin: '0 auto', }}>
+      
+      <h1>Welcome to React</h1>
+
+    </div>
+  )
+}
+
+export default App
